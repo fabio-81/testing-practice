168e5f4b6774edab46220d59d0756a34bb55f401
Fabio <fabiog@live.com.pt>
2020-03-30 22:05:31 -0400

    defined the sum function

---
+++ 01-unit/01-sum.js
@@ -0,0 +1,42 @@
+// ==================================================
+// DEFINE FUNCTION(S)
+// ==================================================
+function sum(num1,num2) {
+return num1 + num2
+}
+
+// ==================================================
+// DEFINE AND EXECUTE TEST CASES
+// ==================================================
+try {
+  // --------------------------------------------------
+  // Test Case 1
+  // --------------------------------------------------
+  // It should return the sum of two numbers.
+  var result = sum(1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1) to be 2. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 2
+  // --------------------------------------------------
+  // It should ignore additional numbers.
+
+  // --------------------------------------------------
+  // Test Case 3
+  // --------------------------------------------------
+  // When invoked with only 1 number, it should return that number.
+
+  // --------------------------------------------------
+  // Test Case 4
+  // --------------------------------------------------
+  // When invoked with no numbers, it should return 0.
+
+  console.log('All tests passed successfully.');
+
+// ==================================================
+// PRINT ERRORS
+// ==================================================
+} catch (e) {
+  console.warn('Whoops, the following test did not pass:');
+  console.error(e.message);
+}
