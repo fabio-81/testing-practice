35519e9dda731c552d4744049792a30ae310c002
Fabio <fabiog@live.com.pt>
2020-04-04 20:40:02 -0400

    fixed braces indentation on line 14

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -11,7 +11,7 @@ function subtract(num1, num2){
 if (num1 === undefined) num1 = 0;
 if (num2 === undefined) num2 = 0;
 return num1 - num2;
-  }
+}
 
 function multiply(num1, num2) {
 if (num1 == undefined) num1 = 0;
