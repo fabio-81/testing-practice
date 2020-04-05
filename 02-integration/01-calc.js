468a3843f40122e795c95a9bbdda2c9ee2aca328
Fabio <fabiog@live.com.pt>
2020-04-04 20:11:56 -0400

    added divide function and added case 'divide'

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -18,15 +18,21 @@ function subtract(num1, num2){
   return num1 * num2;
   }
   
+  function divide(num1, num2) {
+    if (num1 == undefined) num1 = 0;
+
+    if (num2 == undefined) num2 = 0;
+  return num1 / num2;
+  }
 
 function calc(operation, num1, num2) {
 switch(operation){
 case 'add': return sum(num1, num2);
 case 'subtract': return subtract(num1, num2);
 case 'multiply': return multiply(num1, num2)
- }
+case 'divide': return divide(num1, num2)
 }
-
+}
 //===============================================
 // DEFINE AND EXECUTE TEST CASES
 // ==================================================
@@ -49,12 +55,13 @@ try {
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
 var result = calc('multiply', 9, 9)
-if (result !== 81){throw new error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result)}
+if (result !== 81) throw new error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result)
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3)
+  if (result !== 3) throw new error('Expected calc("divide", 9, 3) to be 3. Received: ' + result)
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
